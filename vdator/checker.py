ffc87c7ae9553c1c15de7e8d86bb91c2af7d947e
werrpy <werrpy@gmail.com>
2019-06-05 17:37:49 -0400

    Fix filename video codec

--- vdator/checker.py
+++ vdator/checker.py
@@ -124,7 +124,9 @@ class Checker():
       # source (ex. BluRay.REMUX)
       release_name += '.' + SOURCE
       # video format (ex. AVC)
-      release_name += '.' + self.mediainfo['video'][0]['format']
+      main_video_title = self.mediainfo['video'][0]['title'].split()
+      if len(main_video_title) >= 1:
+        release_name += '.' + main_video_title[0]
       main_audio_title = self.mediainfo['audio'][0]['title'].split(' / ')
       if len(main_audio_title) >= 2:
         # audio codec name for title (ex. DTS-HD.MA)
