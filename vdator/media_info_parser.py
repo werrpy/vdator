e166e499cdd0202daf7070ed8b1888a28ee8ec96
werrpy <werrpy@gmail.com>
2020-12-24 22:52:40 -0500

    (Refactor) clean up mediainfo parser 2

--- vdator/media_info_parser.py
+++ vdator/media_info_parser.py
@@ -14,24 +14,13 @@ class MediaInfoParser():
       .lower()
 
   def parse(self, text):
-    mediainfo = {
-      'general' : list(),
-      'video' : list(),
-      'audio' : list(),
-      'text' : list(),
-      'menu' : list()
-    }
-
+    mediainfo_sections = ['general', 'video', 'audio', 'text', 'menu']
+    # dictionary of lists for mediainfo data
+    mediainfo = dict((k, list()) for k in mediainfo_sections)
     # starts at 0 on first loop
-    section_index = {
-      'general' : -1,
-      'video' : -1,
-      'audio' : -1,
-      'text' : -1,
-      'menu' : -1
-    }
-
-    sect = None
+    section_index = dict((k, -1) for k in mediainfo_sections)
+    # current mediainfo section
+    curr_sect = None
 
     # skip blank lines
     text_list = list(filter(None, text))
@@ -39,26 +28,22 @@ class MediaInfoParser():
     for l in text_list:
       # new section of mediainfo
       section_word = l.strip().split()[0].strip().lower()
-      if section_word in ['general', 'video', 'audio', 'text', 'menu']:
+      if section_word in mediainfo_sections:
         # track current section
-        sect = section_word
+        curr_sect = section_word
         # increment index
         section_index[section_word] += 1
-        if section_word == 'menu':
-          # store new list for chapters
-          mediainfo[section_word].append(list())
-        else:
-          # store new dictionary
-          mediainfo[section_word].append(dict())
+        # store new list for chapters, and new dictionary for other sections
+        mediainfo[section_word].append(list() if section_word == 'menu' else dict())
         continue
 
       # split mediainfo data line
       curr = l.split(' : ', 1)
 
-      if sect in ['general', 'video', 'audio', 'text'] and len(curr) >= 2:
+      if curr_sect in ['general', 'video', 'audio', 'text'] and len(curr) >= 2:
         # assign section to dictionary
-        mediainfo[sect][section_index[sect]][self.format_key(curr[0])] = curr[1]
-      elif sect == 'menu':
+        mediainfo[curr_sect][section_index[curr_sect]][self.format_key(curr[0])] = curr[1]
+      elif curr_sect == 'menu':
         # parse chapters
         chapter = dict()
         if len(curr) >= 1:
@@ -73,6 +58,6 @@ class MediaInfoParser():
           else:
             # no language, just store title
             chapter['title'] = curr[1]
-        mediainfo['menu'][section_index[sect]].append(chapter)
+        mediainfo['menu'][section_index[curr_sect]].append(chapter)
 
     return mediainfo
