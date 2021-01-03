e28790e68ac411c1f6984a221ea06a0b23740987
werrpy <werrpy@gmail.com>
2021-01-03 02:34:15 -0500

    update api

--- vdator/api.py
+++ vdator/api.py
@@ -2,6 +2,9 @@
 Experimental REST API
 
 > python3 api.py
+GET http://127.0.0.1:5000/paste?url=[ENCODED_URL_HERE]
+{"reply":"..."}
+
 GET http://127.0.0.1:5000/parse?url=[ENCODED_URL_HERE]
 {"reply":"..."}
 """
@@ -44,8 +47,39 @@ checker = Checker(codecs_parser, source_detector, reporter)
 
 app = Flask(__name__)
 
-@app.route('/parse', methods=['GET'])
-def get_reply():
+@app.route('/paste', methods=['GET'])
+def parse_url():
+    """
+    This is the language awesomeness API
+    Call this api passing a language name and get back its features
+    ---
+    tags:
+      - Awesomeness Language API
+    parameters:
+      - name: url
+        in: path
+        type: string
+        required: true
+        description: The pastebin url
+    responses:
+      200:
+        description: A language with its awesomeness
+        schema:
+          id: awesome
+          properties:
+            language:
+              type: string
+              description: The language name
+              default: Lua
+            features:
+              type: array
+              description: The awesomeness list
+              items:
+                type: string
+              default: ["perfect", "simple", "lovely"]
+
+    """
+
     url = unquote(request.args.get('url'))
 
     paste = url_parser.get_paste(url)
@@ -72,5 +106,39 @@ def get_reply():
       reply += "\n[ERROR] vdator failed to parse\n"
 
     return jsonify({'reply': reply})
+
+@app.route('/text', methods=['POST'])
+def parse_text():
+    '''
+    POST http://127.0.0.1:5000/text
+    Body, raw
+    [INSERT TEXT HERE]
+    '''
+
+    text = request.get_data().decode("utf-8")
+    bdinfo, mediainfo, eac3to = paste_parser.parse(text)
+    
+    reply = ""
+
+    try:
+      # parse mediainfo
+      mediainfo = mediainfo_parser.parse(mediainfo)
+
+      # setup/reset reporter
+      reporter.setup()
+      # setup checker
+      checker.setup(bdinfo, mediainfo, eac3to, 'remux-bot')
+
+      # run all checks
+      reply += checker.run_checks()
+      
+      # report
+      reply += reporter.display_report()
+    except:
+      traceback.print_exc()
+      reply += "\n[ERROR] vdator failed to parse\n"
+
+    return jsonify({'reply': reply})
+
 
 app.run()
