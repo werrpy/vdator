f43d3a4037a1dea8ef805c3f4851e71dcbc7cf1e
werrpy <werrpy@gmail.com>
2021-05-29 13:19:25 -0400

    Update api

--- vdator/api.py
+++ vdator/api.py
@@ -2,20 +2,16 @@
 Experimental REST API
 
 > python3 api.py
-GET http://127.0.0.1:5000/paste?url=[ENCODED_URL_HERE]
-{"reply":"..."}
-
-GET http://127.0.0.1:5000/parse?url=[ENCODED_URL_HERE]
+POST http://127.0.0.1:5000/text
+    Body, raw
+    [INSERT TEXT HERE]
+    
 {"reply":"..."}
 """
 
 import json, os, traceback
 from urllib.parse import unquote
 from flask import Flask, jsonify, request
-
-# APIs
-import discord
-from discord.utils import get
 
 # parsers
 from helpers import balanced_blockquotes, split_string
@@ -47,66 +43,6 @@ checker = Checker(codecs_parser, source_detector, reporter)
 
 app = Flask(__name__)
 
-@app.route('/paste', methods=['GET'])
-def parse_url():
-    """
-    This is the language awesomeness API
-    Call this api passing a language name and get back its features
-    ---
-    tags:
-      - Awesomeness Language API
-    parameters:
-      - name: url
-        in: path
-        type: string
-        required: true
-        description: The pastebin url
-    responses:
-      200:
-        description: A language with its awesomeness
-        schema:
-          id: awesome
-          properties:
-            language:
-              type: string
-              description: The language name
-              default: Lua
-            features:
-              type: array
-              description: The awesomeness list
-              items:
-                type: string
-              default: ["perfect", "simple", "lovely"]
-
-    """
-
-    url = unquote(request.args.get('url'))
-
-    paste = url_parser.get_paste(url)
-    bdinfo, mediainfo, eac3to = paste_parser.parse(paste)
-    
-    reply = "<" + url + ">" + "\n"
-
-    try:
-      # parse mediainfo
-      mediainfo = mediainfo_parser.parse(mediainfo)
-
-      # setup/reset reporter
-      reporter.setup()
-      # setup checker
-      checker.setup(bdinfo, mediainfo, eac3to, 'remux-bot')
-
-      # run all checks
-      reply += checker.run_checks()
-      
-      # report
-      reply += reporter.display_report()
-    except:
-      traceback.print_exc()
-      reply += "\n[ERROR] vdator failed to parse\n"
-
-    return jsonify({'reply': reply})
-
 @app.route('/text', methods=['POST'])
 def parse_text():
     '''
@@ -115,28 +51,43 @@ def parse_text():
     [INSERT TEXT HERE]
     '''
 
-    text = request.get_data().decode("utf-8")
-    bdinfo, mediainfo, eac3to = paste_parser.parse(text)
-    
     reply = ""
 
     try:
-      # parse mediainfo
-      mediainfo = mediainfo_parser.parse(mediainfo)
-
-      # setup/reset reporter
-      reporter.setup()
-      # setup checker
-      checker.setup(bdinfo, mediainfo, eac3to, 'remux-bot')
-
-      # run all checks
-      reply += checker.run_checks()
-      
-      # report
-      reply += reporter.display_report()
+        text = request.get_data().decode("utf-8")
+        bdinfo, mediainfo, eac3to = paste_parser.parse(text)
     except:
-      traceback.print_exc()
-      reply += "\n[ERROR] vdator failed to parse\n"
+        traceback.print_exc()
+        reply += reporter.print_report("fail", "Failed to get paste")
+    else:
+        try:
+            # parse mediainfo
+            mediainfo = mediainfo_parser.parse(mediainfo)
+        except:
+            traceback.print_exc()
+            reply += reporter.print_report("fail", "Mediainfo parser failed")
+        else:
+            # setup/reset reporter
+            reporter.setup()
+            try:
+                # setup checker
+                checker.setup(bdinfo, mediainfo, eac3to, 'remux-bot')
+            except:
+                traceback.print_exc()
+                reply += reporter.print_report(
+                    "fail", "vdator failed to setup checker"
+                )
+            else:
+                try:
+                    reply += checker.run_checks()
+                except:
+                    traceback.print_exc()
+                    reply += reporter.print_report(
+                        "fail", "vdator failed to parse"
+                    )
+    # report
+    reply += "> **Report**\n"
+    reply += reporter.display_report()
 
     return jsonify({'reply': reply})
 
